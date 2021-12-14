a762aad114c3c9617b192404d0a07bb4635b5d15
sjmoghe <sjmoghe@ncsu.edu>
2021-12-14 00:09:59 -0500

    working auth0 application

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const cors = require('cors');
+const db = require("./db/conn");
+const { ObjectID } = require('bson');
+const dbName = "db-name";
+const collectionName = "users";
+const API_KEY = process.env.API_KEY;
+const PORT = process.env.PORT || 3001;
